d1f61a67b55d20a096d2a41cedc2ce60cb2f1e07
Farzaneh Fallahpour <farzanehhh.fl@gmail.com>
2021-08-03 20:37:42 +0430

    fix bug

--- script.js
+++ script.js
@@ -20,26 +20,6 @@ let tr, td, btn, icon, lengthOfArray;
 
 
 
-<<<<<<< Updated upstream
-// let saveArray = localStorage.getItem(`saveArrayInLocal`);
-// let saveNumberContact = localStorage.getItem('saveNumbContactInLocal');
-// let saveObjContact = localStorage.getItem(`saveArrayInLocal${numberContact}`);
-
-// if (saveObjContact && saveNumberContact) {
-//     // arr = JSON.parse(saveArray);
-//     numberContact = JSON.parse(saveNumberContact);
-//     obj = JSON.parse(saveObjContact);
-//     // saveInLocalStrage();
-//     obj = {};
-// } else {
-//     numberContact = 0;
-//     obj = {};
-//     arr = [];
-// }
-numberContact = 0;
-obj = {};
-arr = [];
-=======
 let saveArray = localStorage.getItem('saveArrayInLocal');
 let saveNumberContact = localStorage.getItem('saveNumbContactInLocal');
 
@@ -57,7 +37,6 @@ if (saveArray && saveNumberContact) {
     arr = [];
 }
 
->>>>>>> Stashed changes
 
 
 inputOfserch.addEventListener('keyup', serch);
@@ -79,15 +58,11 @@ function checkValid() {
     obj.nameInputValue = nameInputValue;
     obj.pohoneNumberInputValue = pohoneNumberInputValue;
     obj.emailInputValue = emailInputValue;
-<<<<<<< Updated upstream
-    obj.id=Number(numberContact);
-=======
     obj.id =arr.length;
     obj.trash = false;
     arr.push(obj);
     localStorage.setItem('saveArrayInLocal', JSON.stringify(arr));
 
->>>>>>> Stashed changes
     if (nameInputValue !== ' ') {
         num++;
         addValue();
@@ -151,7 +126,6 @@ function addValue() {
 
     if (num === 3 && obj.trash === false) {
         tr = document.createElement('tr');
-        tr.setAttribute('id', `${numberContact}`)
         for (let i = 0; i < 4; i++) {
             td = document.createElement('td');
             tr.appendChild(td);
@@ -162,69 +136,26 @@ function addValue() {
             if (i == 0) {
                 td.innerHTML = obj.nameInputValue;
                 td.classList.add('td-text');
-                nameInput.value = '';
+                nameInput.value = ''
             }
             if (i == 1) {
                 td.innerHTML = obj.pohoneNumberInputValue;
                 td.classList.add('td-text');
-                pohoneNumberInput.value = '';
+                pohoneNumberInput.value = ''
             }
             if (i == 2) {
                 td.innerHTML = obj.emailInputValue;
                 td.classList.add('td-text');
-                emailInput.value = '';
+                emailInput.value = ''
             }
             if (i == 3) {
                 icon = document.createElement('i');
-<<<<<<< Updated upstream
-                icon.setAttribute('class', 'far fa-trash-alt');
-                // icon.setAttribute('id', `${numberContact}`);
-                btn.appendChild(icon);
-                td.appendChild(btn);
-                td.classList.add('td-trash');
-                td.addEventListener('click', clearDataItem);
-                console.log(td);
-
-                function clearDataItem() {
-                    // console.log(Number(e.target.id))
-                    // localStorage.removeItem(`saveArrayInLocal${Number(e.target.id)+1}`)
-                    // if (e.target === icon) {
-                    //     // tr.style.display = 'none'
-                    //     // document.getElementsByTagName("tr")[Number(e.target.id)].remove();
-                    //     console.log(icon)
-                    //     console.log(Number(e.target.id))
-                    // }else{
-                    //     console.log('hi')
-                    // }
-                    console.log('hi')
-                    tr.style.display = 'none'
-                    // console.log(e.target)
-                    // console.log(icon)
-                    // // tr.classList.add('hidden')
-                    // // if (e.target === modal) {
-                    // //     modal.style.display = "none";
-                    // // }
-                    // if (numberContact >= 1) {
-                    //     numberContact--;
-                    //     numberOfContact.value = numberContact;
-                    //     arr = arr.splice(Number(e.target.id), 1);
-                    //     localStorage.setItem('saveArrayInLocal', JSON.stringify(arr));
-                    //     localStorage.setItem('saveNumbContactInLocal', JSON.stringify(numberContact));
-                    // } else {
-                    //     e.preventDefault();
-                    // }
-
-                    // console.log(numberContact, Number(e.target.id), 'hi')
-
-                }
-=======
                 icon.classList.add('options');
                 icon.setAttribute('class', 'far fa-trash-alt');
                 icon.setAttribute('id', arr.length-1);
                 icon.setAttribute('job', "delete")
                 td.appendChild(icon)
                 td.classList.add('td-trash');
->>>>>>> Stashed changes
             }
 
         }
@@ -232,17 +163,8 @@ function addValue() {
         num = 0;
         numberContact++;
         numberOfContact.value = numberContact;
-<<<<<<< Updated upstream
-        arr.push(obj);
-        // localStorage.setItem(`saveArrayInLocal`, JSON.stringify(arr));
-        // localStorage.setItem(`saveArrayInLocal${numberContact}`, JSON.stringify(obj));
-        // localStorage.setItem('saveNumbContactInLocal', JSON.stringify(numberContact));
-        console.log(arr)
-        obj = {}
-=======
         localStorage.setItem('saveNumbContactInLocal', JSON.stringify(numberContact));
         obj = {};
->>>>>>> Stashed changes
     }
 
 }
@@ -267,11 +189,7 @@ function serch() {
                 if (txtValue.indexOf(inputOfValue) > -1) {
                     trr[i].style.display = '';
                 } else {
-<<<<<<< Updated upstream
-                    trr[i].style.display = 'none';
-=======
                     trr[i].style.display = 'none'
->>>>>>> Stashed changes
                 }
             }
         }
@@ -294,58 +212,6 @@ function serch() {
 }
 
 
-<<<<<<< Updated upstream
-// function saveInLocalStrage() {
-//     for (let j = 0; j < numberContact; j++) {
-
-//         tr = document.createElement('tr');
-//         for (let i = 0; i < 4; i++) {
-//             td = document.createElement('td');
-//             tr.appendChild(td);
-//             td.classList.add('td');
-//             if (i == 1 || i == 2 || i == 3) {
-//                 td.style.borderLeft = '1px solid #981a00';
-//             }
-//             if (i == 0) {
-//                 td.innerHTML = arr[j].nameInputValue;
-//                 td.classList.add('td-text');
-//                 nameInput.value = ''
-//             }
-//             if (i == 1) {
-//                 td.innerHTML = arr[j].pohoneNumberInputValue;
-//                 td.classList.add('td-text');
-//                 pohoneNumberInput.value = '';
-//             }
-//             if (i == 2) {
-//                 td.innerHTML = arr[j].emailInputValue;
-//                 td.classList.add('td-text');
-//                 emailInput.value = ''
-//             }
-//             if (i == 3) {
-//                 btn = document.createElement('button');
-//                 btn.classList.add('options')
-//                 btn.setAttribute('id', `${j}`);
-//                 icon = document.createElement('i');
-//                 icon.setAttribute('class', 'far fa-trash-alt')
-//                 btn.appendChild(icon)
-//                 td.appendChild(btn)
-//                 td.classList.add('td-trash');
-//                 btn.addEventListener('click', clearData);
-
-//                 function clearData() {
-//                     tr.classList.add('hidden')
-//                     numberContact--;
-//                     numberOfContact.value = numberContact;
-//                 }
-//             }
-
-//         }
-//         table.appendChild(tr);
-//         numberOfContact.value = numberContact;
-
-//     }
-// }
-=======
 function saveInLocalStrage() {
     console.log(arr)
     for (let j = 0; j < lengthOfArray; j++) {
@@ -391,7 +257,6 @@ function saveInLocalStrage() {
 
     }
 }
->>>>>>> Stashed changes
 
 
 function clearAllValueInLocal() {
